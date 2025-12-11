d139eaa5b7614b5b80a1084c2e521b98216fd652
TF-Polygon <zxzx2237@naver.com>
2025-12-11 21:07:03 +0900

    first push

---
+++ model.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import sys
+import os
+
+from glob import glob
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+
+class Conv(nn.Module):
+    def __init__(self, out_channels, init_channels=3, input_size=6):
+        super(Conv, self).__init__()
+
+        def conv_block(in_channels, out_channels):
+            return nn.Sequential(
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Dropout2d(0.3)
+            )
+        
+        self.conv_features = nn.Sequential(
+            conv_block(init_channels, 32),
+            conv_block(32, 64),
+            conv_block(64, 128),
+            nn.Conv2d(128, out_channels, kernel_size=1)
+        )
+        
+        self.output_H = input_size // (2 * 2 * 2)
+        self.output_W = input_size // (2 * 2 * 2)
+    
+    def forward(self, x):
+        x = self.conv_features(x)
+        return x
+    
+class MultiFeatureFusion(nn.Module):
+    def __init__(self, input_size, embed_dim=256, hidden_dim=512, num_features=1, num_classes=3):
+        super(MultiFeatureFusion, self).__init__()
+
+        self.embed_dim = embed_dim
+        dim_per_cnn = embed_dim // num_features
+
+        if num_features == 1:
+            self.cnn_f1 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+            
+        elif num_features == 2:
+            self.cnn_f1 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+            self.cnn_f2 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+
+        else:
+            self.cnn_f1 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+            self.cnn_f2 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+            self.cnn_f3 = Conv(input_size=input_size, out_channels=dim_per_cnn)
+
+        self.f_h = self.cnn_f1.output_H
+        self.f_w = self.cnn_f1.output_W
+    
+        total_in_channels = dim_per_cnn * num_features
+        self.fusion_block = nn.Sequential(
+            nn.Conv2d(total_in_channels, hidden_dim, kernel_size=1),
+            nn.ReLU(),
+        )
+
+        final_vec_size = hidden_dim * self.f_h * self.f_w
+
+        self.classifier = nn.Sequential(
+            nn.Linear(final_vec_size, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(hidden_dim, num_classes)
+        )
+        
+    def forward(self, f_1, f_2=None, f_3=None):
+        f_list = []
+        if f_2 == None and f_3 == None:
+            f1 = self.cnn_f1(f_1)
+            f_list.append(f1)
+
+        elif f_2 != None and f_3 == None:            
+            f1 = self.cnn_f1(f_1)
+            f2 = self.cnn_f2(f_2)
+            f_list.append(f1)
+            f_list.append(f2)
+
+        else:
+            f1 = self.cnn_f1(f_1)
+            f2 = self.cnn_f2(f_2)
+            f3 = self.cnn_f3(f_3)
+            f_list.append(f1)
+            f_list.append(f2)
+            f_list.append(f3)
+
+        x = torch.cat(f_list, dim=1)
+        x = self.fusion_block(x)
+        x = x.flatten(start_dim=1)
+        
+        return self.classifier(x)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def single_feature_model(input_size):
+    return MultiFeatureFusion(input_size=input_size, num_features=1)
+
+def double_features_model(input_size):
+    return MultiFeatureFusion(input_size=input_size, num_features=2)
+
+def multi_features_model(input_size):
+    return MultiFeatureFusion(input_size=input_size, num_features=3)
+
+if __name__ == '__main__':
+    input_size = 24 
+
+    img1 = torch.randn(6, 3, input_size, input_size)
+    img2 = torch.randn(6, 3, input_size, input_size)
+    img3 = torch.randn(6, 3, input_size, input_size)
+
+    single_model = single_feature_model(input_size)
+    double_model = double_features_model(input_size)
+    _multi_model = multi_features_model(input_size)
+
+    logits = single_model(img1)
+    print(logits.shape)
+    print(count_parameters(single_model))
+
+    logits = double_model(img1, img2)
+    print(logits.shape)
+    print(count_parameters(double_model))
+
+    logits = _multi_model(img1, img2, img3)
+    print(logits.shape)
+    print(count_parameters(_multi_model))
---
+++ train.py
@@ -0,0 +1,473 @@
+import os
+import numpy as np
+from datetime import datetime
+
+import sys
+import torch
+import argparse
+import pandas as pd
+from tqdm import tqdm
+
+from torch import nn, optim
+from torchvision import datasets, transforms
+
+from torch.utils.data import Dataset, DataLoader, TensorDataset, random_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, precision_recall_curve
+
+from model import single_feature_model, double_features_model, multi_features_model
+
+def get_model(num_features, input_size):
+    if num_features == 1:
+        return single_feature_model(input_size)
+    elif num_features == 2:
+        return double_features_model(input_size)
+    elif num_features == 3:
+        return multi_features_model(input_size)
+
+def export(model, train_logs, valid_logs, test_results=None):
+    train_data_path = 'train_data'
+    weight_path = 'weights'
+    os.makedirs(train_data_path, exist_ok=True)
+    os.makedirs(weight_path, exist_ok=True)
+    weight_name = datetime.now().strftime("%m-%d_%H-%M")
+    torch.save(model.state_dict(), os.path.join(weight_path, weight_name + ".pt"))
+    log_df = pd.DataFrame(train_logs)
+    log_valdf = pd.DataFrame(valid_logs)
+
+    log_df.to_csv(os.path.join(train_data_path, weight_name + "_train.csv"), index=False)
+    log_valdf.to_csv(os.path.join(train_data_path, weight_name + "_valid.csv"), index=False)    
+
+    if test_results:
+        log_testdf = pd.DataFrame(test_results)
+        log_testdf.to_csv(os.path.join(train_data_path, weight_name + "_test.csv"), index=False)
+        print(f"Successfully save training results! filename: [{weight_name}_train.csv, {weight_name}_valid.csv, {weight_name}_test.csv]")     
+        return 
+    
+    print(f"Successfully save training results! filename: [{weight_name}_train.csv, {weight_name}_valid.csv]") 
+
+def dataloader(num_features, batch_size, path):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomErasing(),
+    ])
+    
+    if num_features == 1:
+        dataset = datasets.ImageFolder(root=path, transform=transform)
+    elif num_features == 2:
+        pass
+    else:
+        pass
+
+    total_size = len(dataset)
+    train_size = int(total_size * 0.7)
+    val_size = int(total_size * 0.15)
+    test_size = total_size - train_size - val_size
+    
+    generator = torch.Generator().manual_seed(42)
+    train_dataset, val_dataset, test_dataset = random_split(
+        dataset, [train_size, val_size, test_size], generator=generator
+    )
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
+
+def train_singlefeature(model, epochs, criterion, optimizer, train_loader, valid_loader, device=torch.device("cuda")):
+    train_logs, valid_logs = [], []
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        all_preds = []
+        all_labels = []
+
+        progress_bar = tqdm(train_loader, desc=f"Train Epoch [{epoch + 1}/{epochs}]", leave=True)
+
+        for images, labels in progress_bar:
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+            acc = accuracy_score(all_labels, all_preds)
+            prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+            rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+            progress_bar.set_postfix({
+                'Loss': f"{loss.item():.4f}",
+                'Acc': f"{acc:.4f}",
+                'Prec': f"{prec:.4f}",
+                'Rec': f"{rec:.4f}"
+            })
+
+        avg_loss = running_loss / len(train_loader)
+        final_acc = accuracy_score(all_labels, all_preds)
+        final_prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+        final_rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+        train_logs.append({
+            "Epoch": epoch + 1,
+            "Train Loss": running_loss / len(train_loader),
+            "Train Accuracy": final_acc,
+            "Train Precision": final_prec,
+            "Train Recall": final_rec
+        })
+
+        print(f"\nEpoch {epoch + 1}, Summary: Loss = {avg_loss:.4f}, Accuracy = {final_acc:.4f}, Precision = {final_prec:.4f}, Recall = {final_rec:.4f}\n", flush=True)
+
+        model.eval()
+        val_preds, val_labels = [], []
+        val_loss = 0.0
+
+        valid_progress_bar = tqdm(valid_loader, desc=f"Validation Epoch [{epoch + 1}/epochs]", leave=True)
+
+        with torch.no_grad():
+            for images, labels in valid_progress_bar:
+                images, labels = images.to(device), labels.to(device)
+
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+
+                preds = torch.argmax(outputs, dim=1)
+                val_preds.extend(preds.cpu().numpy())
+                val_labels.extend(labels.cpu().numpy())
+
+                valid_progress_bar.set_postfix({
+                    'Loss': f"{loss.item():.4f}",
+                    'Acc': f"{accuracy_score(val_labels, val_preds):.4f}",
+                    'Prec': f"{precision_score(val_labels, val_preds, average='macro', zero_division=0):.4f}",
+                    'Rec': f"{recall_score(val_labels, val_preds, average='macro', zero_division=0):.4f}"
+                })
+
+        val_acc = accuracy_score(val_labels, val_preds)
+        val_prec = precision_score(val_labels, val_preds, average='macro', zero_division=0)
+        val_rec = recall_score(val_labels, val_preds, average='macro', zero_division=0)
+        avg_val_loss = val_loss / len(valid_loader)
+        print(f"✅ Validation Summary: Loss = {avg_val_loss:.4f}, Acc = {val_acc:.4f}, Prec = {val_prec:.4f}, Rec = {val_rec:.4f}\n")
+        
+        valid_logs.append({
+            "Epoch": epoch + 1,
+            "Val Loss": val_loss / len(valid_loader),
+            "Val Accuracy": val_acc,
+            "Val Precision": val_prec,
+            "Val Recall": val_rec
+        })
+    
+    return train_logs, valid_logs
+
+def train_doublefeatures(model, epochs, criterion, optimizer, train_loader, val_loader, device=torch.device("cuda")):
+    train_logs, valid_logs = [], []
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        all_preds = []
+        all_labels = []
+
+        progress_bar = tqdm(train_loader, desc=f"Train Epoch [{epoch + 1}/{epochs}]", leave=True)
+
+        for ch1, ch2, labels in progress_bar:
+            ch1 = ch1.to(device)
+            ch2 = ch2.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(ch1, ch2)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+            acc = accuracy_score(all_labels, all_preds)
+            prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+            rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+            progress_bar.set_postfix({
+                'Loss': f"{loss.item():.4f}",
+                'Acc': f"{acc:.4f}",
+                'Prec': f"{prec:.4f}",
+                'Rec': f"{rec:.4f}"
+            })
+
+        avg_loss = running_loss / len(train_loader)
+        final_acc = accuracy_score(all_labels, all_preds)
+        final_prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+        final_rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+        train_logs.append({
+            "Epoch": epoch + 1,
+            "Train Loss": running_loss / len(train_loader),
+            "Train Accuracy": final_acc,
+            "Train Precision": final_prec,
+            "Train Recall": final_rec
+        })
+
+        print(f"\nEpoch {epoch + 1}, Summary: Loss = {avg_loss:.4f}, Accuracy = {final_acc:.4f}, Precision = {final_prec:.4f}, Recall = {final_rec:.4f}\n", flush=True)
+
+        model.eval()
+        val_preds, val_labels = [], []
+        val_loss = 0.0
+
+        valid_progress_bar = tqdm(val_loader, desc=f"Validation Epoch [{epoch + 1}/epochs]", leave=True)
+
+        with torch.no_grad():
+            for ch1, ch2, labels in valid_progress_bar:
+                ch1 = ch1.to(device)
+                ch2 = ch2.to(device)
+                labels = labels.to(device)
+
+                outputs = model(ch1, ch2)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+
+                preds = torch.argmax(outputs, dim=1)
+                val_preds.extend(preds.cpu().numpy())
+                val_labels.extend(labels.cpu().numpy())
+
+                valid_progress_bar.set_postfix({
+                    'Loss': f"{loss.item():.4f}",
+                    'Acc': f"{accuracy_score(val_labels, val_preds):.4f}",
+                    'Prec': f"{precision_score(val_labels, val_preds, average='macro', zero_division=0):.4f}",
+                    'Rec': f"{recall_score(val_labels, val_preds, average='macro', zero_division=0):.4f}"
+                })
+
+        val_acc = accuracy_score(val_labels, val_preds)
+        val_prec = precision_score(val_labels, val_preds, average='macro', zero_division=0)
+        val_rec = recall_score(val_labels, val_preds, average='macro', zero_division=0)
+        avg_val_loss = val_loss / len(val_loader)
+        print(f"✅ Validation Summary: Loss = {avg_val_loss:.4f}, Acc = {val_acc:.4f}, Prec = {val_prec:.4f}, Rec = {val_rec:.4f}\n")
+        
+        valid_logs.append({
+            "Epoch": epoch + 1,
+            "Val Loss": val_loss / len(val_loader),
+            "Val Accuracy": val_acc,
+            "Val Precision": val_prec,
+            "Val Recall": val_rec
+        })
+
+    return train_logs, valid_logs
+
+def train_multifeatures(model, epochs, criterion, optimizer, train_loader, val_loader, device=torch.device("cuda")):
+    train_logs, valid_logs = [], []
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        all_preds = []
+        all_labels = []
+
+        progress_bar = tqdm(train_loader, desc=f"Train Epoch [{epoch + 1}/{epochs}]", leave=True)
+
+        for ch1, ch2, ch3, labels in progress_bar:
+            ch1 = ch1.to(device)
+            ch2 = ch2.to(device)
+            ch3 = ch3.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(ch1, ch2, ch3)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+            acc = accuracy_score(all_labels, all_preds)
+            prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+            rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+            progress_bar.set_postfix({
+                'Loss': f"{loss.item():.4f}",
+                'Acc': f"{acc:.4f}",
+                'Prec': f"{prec:.4f}",
+                'Rec': f"{rec:.4f}"
+            })
+
+        avg_loss = running_loss / len(train_loader)
+        final_acc = accuracy_score(all_labels, all_preds)
+        final_prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
+        final_rec = recall_score(all_labels, all_preds, average='macro', zero_division=0)
+
+        train_logs.append({
+            "Epoch": epoch + 1,
+            "Train Loss": running_loss / len(train_loader),
+            "Train Accuracy": final_acc,
+            "Train Precision": final_prec,
+            "Train Recall": final_rec
+        })
+
+        print(f"\nEpoch {epoch + 1}, Summary: Loss = {avg_loss:.4f}, Accuracy = {final_acc:.4f}, Precision = {final_prec:.4f}, Recall = {final_rec:.4f}\n", flush=True)
+
+        model.eval()
+        val_preds, val_labels = [], []
+        val_loss = 0.0
+
+        valid_progress_bar = tqdm(val_loader, desc=f"Validation Epoch [{epoch + 1}/epochs]", leave=True)
+
+        with torch.no_grad():
+            for ch1, ch2, ch3, labels in valid_progress_bar:
+                ch1 = ch1.to(device)
+                ch2 = ch2.to(device)
+                ch3 = ch3.to(device)
+                labels = labels.to(device)
+
+                outputs = model(ch1, ch2, ch3)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+
+                preds = torch.argmax(outputs, dim=1)
+                val_preds.extend(preds.cpu().numpy())
+                val_labels.extend(labels.cpu().numpy())
+
+                valid_progress_bar.set_postfix({
+                    'Loss': f"{loss.item():.4f}",
+                    'Acc': f"{accuracy_score(val_labels, val_preds):.4f}",
+                    'Prec': f"{precision_score(val_labels, val_preds, average='macro', zero_division=0):.4f}",
+                    'Rec': f"{recall_score(val_labels, val_preds, average='macro', zero_division=0):.4f}"
+                })
+
+        val_acc = accuracy_score(val_labels, val_preds)
+        val_prec = precision_score(val_labels, val_preds, average='macro', zero_division=0)
+        val_rec = recall_score(val_labels, val_preds, average='macro', zero_division=0)
+        avg_val_loss = val_loss / len(val_loader)
+        print(f"✅ Validation Summary: Loss = {avg_val_loss:.4f}, Acc = {val_acc:.4f}, Prec = {val_prec:.4f}, Rec = {val_rec:.4f}\n")
+        
+        valid_logs.append({
+            "Epoch": epoch + 1,
+            "Val Loss": val_loss / len(val_loader),
+            "Val Accuracy": val_acc,
+            "Val Precision": val_prec,
+            "Val Recall": val_rec
+        })
+    
+    return train_logs, valid_logs
+
+def test_singlefeature(model, test_loader, criterion, device):
+    test_preds, test_labels, test_results = [], [], []
+    test_loss = 0.0
+    
+    print("***** Start testing the model")
+    test_progress_bar = tqdm(test_loader, desc=f"Testing the model", leave=True)
+    with torch.no_grad():
+        for images, labels in test_progress_bar:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+
+            preds = torch.argmax(outputs, dim=1)
+            test_preds.extend(preds.cpu().numpy())
+            test_labels.extend(labels.cpu().numpy())
+
+    test_acc = accuracy_score(test_labels, test_preds)
+    test_prec = precision_score(test_labels, test_preds, average='macro', zero_division=0)
+    test_rec = recall_score(test_labels, test_preds, average='macro', zero_division=0)
+    test_results.append({
+        "Test Accuracy": test_acc,
+        "Test Precision": test_prec,
+        "Test Recall": test_rec
+    })
+
+    print(f"\n🏁 Test Set Evaluation Result:")
+    print(f"Loss = {test_loss / len(test_loader):.4f}, Accuracy = {test_acc:.4f}, Precision = {test_prec:.4f}, Recall = {test_rec:.4f} ")
+
+    return test_labels, test_preds, test_results
+
+def main(args):
+    model = get_model(args.num_features, args.input_size)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+
+    train_loader, valid_loader, test_loader = dataloader(args.num_features, args.batch_size, args.dataset)
+
+    if args.num_features == 1:
+        train_logs, valid_logs = train_singlefeature(
+            model=model,
+            epochs=args.epochs,
+            criterion=criterion,
+            optimizer=optimizer,
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+        )
+
+        model.eval()
+        test_labels, test_preds, test_results = test_singlefeature(
+            model=model,
+            test_loader=test_loader,
+            criterion=criterion,
+            device=device
+        )
+        
+        export(
+            model=model,
+            train_logs=train_logs,
+            valid_logs=valid_logs,
+            test_results=test_results
+        )
+
+    elif args.num_features == 2:
+        train_logs, valid_logs = train_doublefeatures(
+            model=model,
+            epochs=args.epochs,
+            criterion=criterion,
+            optimizer=optimizer,
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+        )    
+
+        export(
+            model=model,
+            train_logs=train_logs,
+            valid_logs=valid_logs
+        )
+
+    elif args.num_features == 3:
+        train_logs, valid_logs = train_multifeatures(
+            model=model,
+            epochs=args.epochs,
+            criterion=criterion,
+            optimizer=optimizer,
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+        )    
+        
+        export(
+            model=model,
+            train_logs=train_logs,
+            valid_logs=valid_logs
+        )
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset', type=str, required=True, help='path to dataset')
+    parser.add_argument('--input_size', type=int, required=True, help='input size of model')
+    parser.add_argument('--num_features', type=int, required=True, default=3, help='Number of features to input')
+    parser.add_argument('--epochs', type=int, required=True, default=10)
+    parser.add_argument('--batch_size', type=int, required=True, default=32)
+    
+    args = parser.parse_args()
+
+    main(args)
