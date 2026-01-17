38815f9baf387607ade61c8fb964178589a70a5c
TF-Polygon <zxzx2237@naver.com>
2026-01-17 18:43:52 +0900

    add the parameter num_classes

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ if __name__ == '__main__':
 
     parser.add_argument('--dataset', type=str, required=True, help='path to dataset')
     parser.add_argument('--input_size', type=int, required=True, help='input size of model')
-    parser.add_argument('--num_features', type=int, required=True, default=3, help='Number of features to input')
+    parser.add_argument('--num_features', type=int, required=True, default=3, help='number of features for fusion')
     parser.add_argument('--epochs', type=int, required=True, default=10)
     parser.add_argument('--batch_size', type=int, required=True, default=32)
     parser.add_argument('--test', type=bool, default=False)
+    parser.add_argument('--num_classes', type=int, default=2, help='number of classes')
     
     args = parser.parse_args()
 
