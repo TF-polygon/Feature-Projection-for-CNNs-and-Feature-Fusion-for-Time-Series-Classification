a818c350bb89c6de83c34387acda6d9d3962f8bc
TF-Polygon <zxzx2237@naver.com>
2026-01-19 16:28:23 +0900

    deleted unusable parameters

--- clustering.py
+++ clustering.py
@@ -25,7 +25,7 @@ def export(kmeans, scaled_data, file_name):
         n_clusters=kmeans.n_clusters
     )
 
-def clustering(data, scaled_data, smoother, n_clusters, symbol='EUR/USD', max_iter=20, random_state=123, n_init=3):
+def clustering(scaled_data, n_clusters, symbol='EURUSD', max_iter=20, random_state=123, n_init=3):
     console.log(f'Start to do clustering {symbol}...')
     with console.status('[bold green] Clustering...') as status:
         kmeans = TimeSeriesKMeans(
@@ -33,7 +33,6 @@ def clustering(data, scaled_data, smoother, n_clusters, symbol='EUR/USD', max_it
             metric="dtw",
             max_iter=max_iter, 
             random_state=random_state,
-            max_iter_barycenter=10,
             n_init=n_init,
             verbose=False
         ).fit(scaled_data)
@@ -42,13 +41,15 @@ def clustering(data, scaled_data, smoother, n_clusters, symbol='EUR/USD', max_it
 
     # start_index_in_original_data = len(data) - len(kmeans.labels_)
     # dates_for_time = data['Date'][start_index_in_original_data : len(data)]
-    time_column_names = [f'Time_{i:02d}' for i in range(1, smoother.Smoother.data.shape[1] + 1)]
-    clustered_data = pd.DataFrame(smoother.Smoother.data, columns=time_column_names)
+    reshaped_data = scaled_data.reshape(scaled_data.shape[0], scaled_data.shape[1])
+    time_column_names = [f'Time_{i:02d}' for i in range(1, reshaped_data.shape[1] + 1)]
+    clustered_data = pd.DataFrame(reshaped_data, columns=time_column_names)
     clustered_data['Cluster_Label'] = kmeans.labels_
     filename = f'clustered_{symbol}_{n_clusters}cls.csv'
-    clustered_data.to_csv(os.path.join('clustered_data', filename), index=False)
+    path = 'data/processed'
+    clustered_data.to_csv(os.path.join(path, filename), index=False)
 
-    export(kmeans, f'{symbol}.npz')
+    export(kmeans, reshaped_data, f'{symbol}.npz')
 
     console.log(f'Successfully saved the clustered data in {filename}.\n\n')
 
@@ -58,6 +59,7 @@ def run(args):
     stride = args.stride
     n_clusters = args.n_clusters
     n_init = args.n_init
+    symbol = args.symbol
 
     smoother = WindowWrapper(LowessSmoother(smooth_fraction=0.6, iterations=1, batch_size=1000), window_shape=window_shape)
     smoother.smooth(data['Close'].values)
@@ -65,11 +67,10 @@ def run(args):
     raw_scaled = TimeSeriesScalerMinMax().fit_transform(strided_data)
 
     clustering(
-        data=data, 
-        scaled_data=raw_scaled, 
-        smoother=smoother, 
+        scaled_data=raw_scaled,
         n_clusters=n_clusters,
-        n_init=n_init
+        n_init=n_init,
+        symbol=symbol
     )
 
 if __name__ == '__main__':
@@ -78,10 +79,11 @@ if __name__ == '__main__':
     parser.add_argument('--path', type=str, required=True, help='path to dataset')
     parser.add_argument('--n_clusters', type=int, required=True, help='$k$: number of clusters')
     parser.add_argument('--window_shape', type=int, required=True, help='$W$: window shape for clustering')
-    parser.add_argument('--symbol', type=str, default='EUR/USD', help='symbol (ex: EUR/USD)')
-    parser.add_argument('--max_iter', type=int, default=10, help='Max Iteration')
-    parser.add_argument('--random_state', type=int, default=123, help='Max Iteration')    
+    parser.add_argument('--symbol', type=str, default='EURUSD', help='symbol (ex: EURUSD)')
+    parser.add_argument('--max_iter', type=int, default=10, help='max iteration')
+    parser.add_argument('--random_state', type=int, default=123, help='random state')    
     parser.add_argument('--n_init', type=int, default=3, help='n_init')
+    parser.add_argument('--stride', type=int, default=12, help='stride for clustering')
 
     args = parser.parse_args()
 
